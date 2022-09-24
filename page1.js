f4a93ea3a7078f50ff5c1959b391ecdd95e5383d
Vo Di Nguyen <dingyen96@gmail.com>
2022-09-24 22:30:09 +0700

    Added page1.js

---
+++ page1.js
@@ -0,0 +1,8 @@
+function func1() {
+    document.getElementById("demo").innerHTML = "Voila!";
+}
+
+function func2() {
+    let x = document.getElementById("num").value;
+    document.getElementById("res").innerHTML = x * 2;
+}
